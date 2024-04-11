4c6fbaff8dfd7bca782f0dae7408b483e8aa2997
Mohamed Sakr <mohamed1.sakr1996@gmail.com>
2024-04-11 21:42:14 +0200

    added react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Link } from 'react-router-dom'
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [value, setValue] = useState('')
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1> React Demo</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      <label htmlFor="input">{value}</label>
-        <input type="text"  value={value} onChange={(e) => setValue(e.target.value)}/>
+        <Link to="/use-state">Use State Example</Link><br />
+        <Link to="/use-effect">Use Effect Example</Link><br />
+        <Link to="/use-ref">Use Ref Example</Link><br />
+        <Link to="/use-context">Use Context Example</Link><br />
+        <Link to="/use-memo">Use memo Example</Link><br />
+        <Link to="/use-reducer">Use Reducer Example</Link><br />
+        <Link to="/use-custom-hook">Use Custom Hook Example</Link><br />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -10,17 +10,22 @@ import UseContextExample from './Components/UseContextExample.jsx'
 import UseMemoExample from './Components/UseMemoExample.jsx'
 import UseReducerExample from './Components/UseReducerExample.jsx'
 import UseCustomHookExample from './Components/UseCustomHookExample.jsx'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // 4 wrap app with context provider
   <UseContextProvider>
-      {/* <App /> */}
-      {/* <UseStateExample /> */}
-      {/* <UseEffectExample /> */}
-      {/* <UseRefExample /> */}
-      {/* <UseContextExample /> */}
-      {/* <UseMemoExample /> */}
-      {/* <UseReducerExample /> */}
-      <UseCustomHookExample />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} />
+        <Route path='/use-state' element={<UseStateExample />} />
+        <Route path='/use-effect' element={<UseEffectExample />} />
+        <Route path='/use-ref' element={<UseRefExample />} />
+        <Route path='/use-context' element={<UseContextExample />} />
+        <Route path='/use-memo' element={<UseMemoExample />} />
+        <Route path='/use-reducer' element={<UseReducerExample />} />
+        <Route path='/use-custom-hook' element={<UseCustomHookExample />} />
+      </Routes>
+    </BrowserRouter>
   </UseContextProvider>
 )
